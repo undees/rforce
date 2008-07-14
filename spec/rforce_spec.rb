a6d5d1c1938c2f141843179ecd1ce613dc3c5ac3
Ian Dees <undees@gmail.com>
2008-07-14 11:12:19 -0700

    Crude cut/paste addition of Wes's faster expat parser.
    --HG--
    branch : trunk

--- spec/rforce_spec.rb
+++ spec/rforce_spec.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
+require 'pp'
 
 describe FlashHash do
   it 'lets you access hash keys with methods' do
@@ -33,6 +34,46 @@ describe 'expand' do
     expand(builder, data)
 
     expanded.should == CreateXml
+  end
+end
+
+describe SoapResponse do
+  before :all do
+    fname = File.join(File.dirname(__FILE__), 'soap-response.xml')
+    @contents = File.open(fname) {|f| f.read}
+
+    @rexml_recs, @expat_recs = [SoapResponse, SoapResponseExpat].map do |klass|
+      results = klass.new(@contents)
+      results[:queryResponse][:result][:records]
+    end
+    
+  end
+
+  it 'turns XML into objects' do
+    @rexml_recs.size.should == 58
+  end
+  
+  it 'loosely matches the expat results' do
+    @expat_recs.size.should == @rexml_recs.size
+    @rexml_recs.each_with_index do |rexml_rec, index|
+      expat_rec = @expat_recs[index]
+      rexml_rec[:Id].each do |id|
+        id.should == expat_rec[:Id]
+      end
+      
+      rexml_rec.keys.should == expat_rec.keys
+      rexml_rec.each do |key|
+        unless key == :Id
+          rexml_rec[key].should == expat_rec[key]
+        end
+      end
+    end
+  end
+  
+  it 'exactly matches the expat results' do
+    pending 'Still have to work out Id tags' do
+      @rexml_recs.should == @expat_recs
+    end
   end
 end
 
