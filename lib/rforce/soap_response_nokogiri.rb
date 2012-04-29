20574234a7efb688bdb6402c2c506061e06e7679
bradrobertson <brad@influitive.com>
2012-04-28 19:57:10 -0500

    ids arent arrays anymore
    numeric/boolean now parsed
    
    records are always an array regardless of length

--- lib/rforce/soap_response_nokogiri.rb
+++ lib/rforce/soap_response_nokogiri.rb
@@ -12,34 +12,54 @@ module RForce
       to_hash(body)
     end
 
-    private
+  private
+
     def to_hash(node)
-      if node.text?
-        stripped = node.text.strip
-        return stripped.empty? ? nil : stripped
-      end
+      return parse_text(text) if node.text?
 
       children = node.children.reject {|c| c.text? && c.text.strip.empty? }
 
       return nil if children.empty?
 
-      return children.first.text.strip if children.first.text?
+      if (child = children.first).text?
+        return parse_text(child.text)
+      end
 
       elements = MethodHash.new
 
       children.each do |elem|
         name = elem.name.split(":").last.to_sym
-        
+
         if !elements[name]
-          elements[name] = to_hash(elem)
+          # anything inside 'records' should be an array
+          elements[name] = elem.name == 'records' ? [to_hash(elem)] : to_hash(elem)
         elsif Array === elements[name]
           elements[name] << to_hash(elem)
         else
+          next if elem.name == "Id" # Id fields are duplicated
           elements[name] = [elements[name]] << to_hash(elem)
         end
       end
-      
+
       return elements.empty? ? nil : elements
+    end
+
+    def parse_text(text)
+      text.strip!
+
+      return nil if text.empty?
+
+      text = Integer(text) rescue text # parse numbers
+
+      boolean?(text) ? boolean(text) : text
+    end
+
+    def boolean(string)
+      string == "true"
+    end
+
+    def boolean?(string)
+      %w{true false}.include?(string)
     end
   end
 end
